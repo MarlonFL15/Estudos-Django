518b30e9a1b8b677a8c5f1f3154c72ab72a0a607
unknown <marlonfleite50@gmail.com>
2021-09-10 15:19:24 -0400

    imprimindo variáveis python nos templates

--- django_templates/app/views.py
+++ django_templates/app/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    nome_empresa = 'Nome da Empresa'
+    descricao_empresa = 'Descrição da empresa'
+
+
+    return render(request, 'index.html', {
+        'nome_empresa':nome_empresa,
+        'descricao_empresa': descricao_empresa
+    })
