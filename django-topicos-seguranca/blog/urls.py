9ad3d8a1b9b4e1b5a1e5769cbf51c53c0cc2ca75
unknown <marlonfleite50@gmail.com>
2021-10-13 13:08:51 -0400

    implementando login e cadastro usando a tabela de autenticação padrão do django

--- django-topicos-seguranca/blog/urls.py
+++ django-topicos-seguranca/blog/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('post/<int:id>', listar_post_id, name='listar_post'),
+    path('cadastrar_usuario/', cadastrar_usuario, name='cadastrar_usuario'),
+    path('logar_usuario/', logar_usuario, name='logar_usuario')
 ]
--- django-topicos-seguranca/blog/views.py
+++ django-topicos-seguranca/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from admin_blog.services import post_service, comentario_service
 from admin_blog.forms import comentario_form
 from admin_blog.entidades.comentario import Comentario
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -22,3 +24,32 @@ def listar_post_id(request, id):
     else:
         form_comentario = comentario_form.ComentarioForm()
     return render(request, 'blog/post.html', {'post': post, 'comentarios': comentarios, 'form_comentario': form_comentario})
+
+def cadastrar_usuario(request):
+    if request.method == "POST":
+        #Utiliza um módulo do django para criar formulário de cadastro de usuário
+        form_usuario = UserCreationForm(request.POST)
+
+        if form_usuario.is_valid():
+            form_usuario.save()
+            return redirect('home')
+    else:
+        form_usuario = UserCreationForm()
+    return render(request, 'usuario/cadastro.html', {'form_usuario':form_usuario})
+
+def logar_usuario(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        usuario = authenticate(request, username=username, password=password)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('home')
+        else:
+            form_login = AuthenticationForm(request.POST)
+    else:
+        form_login = AuthenticationForm()
+
+    return render(request, 'usuario/login.html', {'form_login':form_login})
