5b586a0570bed412a30b96e6f9bf90997faa42c2
unknown <marlonfleite50@gmail.com>
2021-10-25 15:49:13 -0400

    alterando estilo do django admin

--- django-admin/django_admin/urls.py
+++ django-admin/django_admin/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
 ]
